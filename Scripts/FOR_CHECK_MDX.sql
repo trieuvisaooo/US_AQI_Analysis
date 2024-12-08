436e4872e34ee137e959c59c390e4bdbe99a2aed
trieuvisaooo <trieulaobao@gmail.com>
2024-12-08 20:16:17 +0700

    mdx: update qs1

---
+++ Scripts/FOR_CHECK_MDX.sql
@@ -0,0 +1,24 @@
+use US_AQI_DDS
+go
+
+-- QS1
+SELECT 
+    s.state_name AS [State Name],
+    d.Year AS [Year], 
+	d.Quarter AS [Quarter],
+	MIN(f.aqi_value) AS [MIN_AQI_VALUE],
+    MAX(f.aqi_value) AS [MAX_AQI_VALUE]
+FROM 
+    FACT_AIR_QUALITY f
+JOIN 
+    dim_date d ON f.date_key = d.date_key
+JOIN 
+    dim_county c ON f.county_key = c.county_key
+JOIN 
+    dim_state s ON c.state_key = s.state_key
+JOIN 
+    dim_parameter p ON f.defining_parameter_key = p.parameter_key
+GROUP BY 
+    d.Year, d.Quarter, s.state_name
+ORDER BY 
+	s.state_name
